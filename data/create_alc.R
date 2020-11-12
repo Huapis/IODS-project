50b829bcbfe9998b3fe004a5812c7d549a80770e
Huapis <73789447+Huapis@users.noreply.github.com>
2020-11-12 20:17:15 +0200

    Add files via upload
    I had problems pushing and pulling from GIT, had to manually upload these files here. 
    >>> C:/Program Files/Git/bin/git.exe push origin HEAD:refs/heads/master
    To https://github.com/Huapis/IODS-project.git
     ! [rejected]        HEAD -> master (non-fast-forward)
    error: failed to push some refs to 'https://github.com/Huapis/IODS-project.git'
    hint: Updates were rejected because the tip of your current branch is behind
    hint: its remote counterpart. Integrate the remote changes (e.g.
    hint: 'git pull ...') before pushing again.
    hint: See the 'Note about fast-forwards' in 'git push --help' for details.

---
+++ data/create_alc.R
@@ -0,0 +1,58 @@
+#Miikka Haapakoski, 12.11.2020.
+#This is a description for the 3rd exercise. 
+library(dplyr)
+library(ggplot2)
+library(GGally)
+library(tidyr)
+
+
+por <- read.table("C:\\Users\\miikk\\Documents\\IODS-project\\data\\student-por.csv", sep = ";", header=TRUE)
+math <- read.table("C:\\Users\\miikk\\Documents\\IODS-project\\data\\student-mat.csv", sep = ";", header=TRUE)
+
+
+por_id <- por %>% mutate(id=1000+row_number()) 
+math_id <- math %>% mutate(id=2000+row_number())
+
+
+free_cols <- c("id","failures","paid","absences","G1","G2","G3")
+
+
+join_cols <- setdiff(colnames(por_id),free_cols)
+
+pormath_free <- por_id %>% bind_rows(math_id) %>% select(one_of(free_cols))
+
+
+pormath <- por_id %>% 
+  bind_rows(math_id) %>%
+ 
+  group_by(.dots=join_cols) %>%  
+  
+  summarise(                                                           
+    n=n(),
+    id.p=min(id),
+    id.m=max(id),
+    failures=round(mean(failures)),     
+    paid=first(paid),                   
+    absences=round(mean(absences)),
+    G1=round(mean(G1)),
+    G2=round(mean(G2)),
+    G3=round(mean(G3))    
+  ) %>%
+
+  filter(n==2, id.m-id.p>650) %>%  
+ 
+  inner_join(pormath_free,by=c("id.p"="id"),suffix=c("",".p")) %>%
+  inner_join(pormath_free,by=c("id.m"="id"),suffix=c("",".m")) %>%
+  ungroup %>% mutate(
+    alc_use = (Dalc + Walc) / 2,
+    high_use = alc_use > 2,
+    cid=3000+row_number()
+  )
+glimpse(pormath)
+
+gather(pormath) %>% glimpse
+
+library(openxlsx)
+write.xlsx(pormath,file="C:\\Users\\miikk\\Documents\\IODS-project\\data\\pormath.xlsx")
+
+pormath <- read_excel("data/pormath.xlsx")
